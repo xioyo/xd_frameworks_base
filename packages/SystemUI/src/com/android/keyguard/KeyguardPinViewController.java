73f9ff14d69dbd080aefc08966182598250fc290
Adnan <adnan@cyngn.com>
2014-08-03 17:00:20 -0700

    Keyguard: Add option to scramble pin layout when unlocking (2/2)
    Squashed:
    Keyguard: Don't disable visibility when scrambling pin.
    Keyguard: Fix scramblepin logic.
    SystemUI: Read PIN scramble setting from current user
    
    Change-Id: I6b3763c05f8dc6d49c62c5dc5c01dce8d18d09ca
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/keyguard/KeyguardPinViewController.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardPinViewController.java
@@ -22,6 +22,8 @@ import android.view.View;
 import android.widget.LinearLayout;
 
 import androidx.constraintlayout.helper.widget.Flow;
+import android.os.UserHandle;
+import android.provider.Settings;
 
 import com.android.internal.util.LatencyTracker;
 import com.android.internal.widget.LockPatternUtils;
@@ -35,6 +37,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.stream.Collectors;
 import java.util.List;
+import java.util.ArrayList;
 
 public class KeyguardPinViewController
         extends KeyguardPinBasedInputViewController<KeyguardPINView> {
@@ -45,6 +48,8 @@ public class KeyguardPinViewController
     private final LockPatternUtils mLockPatternUtils;
     private final View mDeleteButton;
     private boolean mDeleteButtonShowing = true;
+
+    private static List<Integer> sNumbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 0);
 
     protected KeyguardPinViewController(KeyguardPINView view,
             KeyguardUpdateMonitor keyguardUpdateMonitor,
@@ -111,6 +116,35 @@ public class KeyguardPinViewController
                 getKeyguardSecurityCallback().reset();
                 getKeyguardSecurityCallback().onCancelClicked();
             });
+        }
+
+        boolean scramblePin = Settings.System.getIntForUser(getContext().getContentResolver(),
+                Settings.System.LOCKSCREEN_PIN_SCRAMBLE_LAYOUT, 0,
+                UserHandle.USER_CURRENT) == 1;
+
+        if (scramblePin) {
+            Collections.shuffle(sNumbers);
+            // get all children who are NumPadKey's
+            LinearLayout container = (LinearLayout) mView.findViewById(R.id.pin_container);
+
+            List<NumPadKey> views = new ArrayList<NumPadKey>();
+            for (int i = 0; i < container.getChildCount(); i++) {
+                if (container.getChildAt(i) instanceof LinearLayout) {
+                    LinearLayout nestedLayout = ((LinearLayout) container.getChildAt(i));
+                    for (int j = 0; j < nestedLayout.getChildCount(); j++){
+                        View view = nestedLayout.getChildAt(j);
+                        if (view.getClass() == NumPadKey.class) {
+                            views.add((NumPadKey) view);
+                        }
+                    }
+                }
+            }
+
+            // reset the digits in the views
+            for (int i = 0; i < sNumbers.size(); i++) {
+                NumPadKey view = views.get(i);
+                view.setDigit(sNumbers.get(i));
+            }
         }
 
         mPostureController.addCallback(mPostureCallback);
