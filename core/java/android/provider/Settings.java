c36b3476e8b7bd6eab946a2a34395a539b57a03b
LuK1337 <priv.luk@gmail.com>
2019-09-20 18:30:03 +0200

    Base: Add navbar layout inversion tuning [1/2]
    * Originally implemented by jesec but since
      then has been reworked by me to use RTL
      instead of replacing left / right buttons.
    
    idoybh:
    * Use existing settings observer instead of tunables
    * Properly add the setting to Settings.java So we can back it up
    * Get rid of useless global var
    
    @/jhonboy121:
    * use NavigationBarModeController to update layout on settings change
    
    Change-Id: I6159f0232ba2ee9f45d2afbba7d11072d902d9ac
    Co-authored-by: Ido Ben-Hur <idoybh2@gmail.com>
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5638,6 +5638,13 @@ public final class Settings {
         public static final String MNMLIST_SUMMARY = "mnmlist_summary";
 
         /**
+         * Whether to inverse keys in navigation bar
+         * Default 0
+         * @hide
+         */
+        public static final String NAVIGATION_BAR_INVERSE = "navigation_bar_inverse";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
