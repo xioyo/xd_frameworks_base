488528a1eebb44bbe9820e76aa29688af3d4177c
Michael W <baddaemon87@gmail.com>
2020-02-05 19:03:21 +0100

    SystemUI: Fix shutter sound
    * When shutter sound for camera is forced on as required in some states,
      (config_camera_sound_forced, set via mcc/mnc), we also want to
      (or should) play it when a screenshot is taken from the preview
      instead of an actual picture
    * This change is loosely based on
      https://android-review.googlesource.com/c/platform/frameworks/base/+/1517742/
      but uses publicly available APIs
    
    Testing:
    - Set config_camera_sound_forced to true and push a build to device
    - Turn down all stream volumes to muted
    - Take screenshot of any normal screen
      -> No sound played
    - Open camera, take screenshot
      -> Sound played
    - Turn up volume and repeat the screenshots
      -> Sound played in all cases
    
    @Stallix - Forward port to A13.
    
    Change-Id: Iacbb577c64f73b79cdfae7fb0487fc6a34ffe41c

--- packages/SystemUI/src/com/android/systemui/screenshot/ScreenshotController.java
+++ packages/SystemUI/src/com/android/systemui/screenshot/ScreenshotController.java
@@ -53,6 +53,7 @@ import android.content.res.Configuration;
 import android.graphics.Bitmap;
 import android.graphics.Insets;
 import android.graphics.Rect;
+import android.hardware.camera2.CameraManager;
 import android.hardware.display.DisplayManager;
 import android.media.AudioAttributes;
 import android.media.AudioSystem;
@@ -62,6 +63,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.os.RemoteException;
+import android.os.SystemProperties;
 import android.os.UserHandle;
 import android.os.VibrationEffect;
 import android.os.Vibrator;
@@ -266,6 +268,8 @@ public class ScreenshotController {
     private final AccessibilityManager mAccessibilityManager;
     private final AudioManager mAudioManager;
     private final ListenableFuture<MediaPlayer> mCameraSound;
+    private final CameraManager mCameraManager;
+    private int mCamsInUse = 0;
     private final ScrollCaptureClient mScrollCaptureClient;
     private final PhoneWindow mWindow;
     private final DisplayManager mDisplayManager;
@@ -387,6 +391,9 @@ public class ScreenshotController {
         // Grab system services needed for screenshot sound
         mAudioManager = (AudioManager) mContext.getSystemService(Context.AUDIO_SERVICE);
         mVibrator = (Vibrator) mContext.getSystemService(Context.VIBRATOR_SERVICE);
+        mCameraManager = (CameraManager) mContext.getSystemService(Context.CAMERA_SERVICE);
+        mCameraManager.registerAvailabilityCallback(mCamCallback,
+                new Handler(Looper.getMainLooper()));
 
         mCopyBroadcastReceiver = new BroadcastReceiver() {
             @Override
@@ -919,6 +926,7 @@ public class ScreenshotController {
     }
 
     private void playShutterSoundIf() {
+        boolean playSound = readCameraSoundForced() && mCamsInUse > 0;
         switch (mAudioManager.getRingerMode()) {
             case AudioManager.RINGER_MODE_SILENT:
                 // do nothing
@@ -930,15 +938,37 @@ public class ScreenshotController {
                 }
                 break;
             case AudioManager.RINGER_MODE_NORMAL:
-                 // Play the shutter sound to notify that we've taken a screenshot
-                 if (Settings.System.getIntForUser(mContext.getContentResolver(),
-                     Settings.System.SCREENSHOT_SHUTTER_SOUND, 1, UserHandle.USER_CURRENT) == 1) {
-                     // Play the shutter sound to notify that we've taken a screenshot
-                     playCameraSound();
-                 }
+                 // in this case we want to play sound even if not forced on
+                 playSound = true;
                  break;
         }
+        // We want to play the shutter sound when it's either forced or
+        // when we use normal ringer mode
+        if (playSound && Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.SCREENSHOT_SHUTTER_SOUND, 1, UserHandle.USER_CURRENT) == 1) {
+            playCameraSound();
+        }
     }
+
+    private CameraManager.AvailabilityCallback mCamCallback =
+            new CameraManager.AvailabilityCallback() {
+        @Override
+        public void onCameraOpened(String cameraId, String packageId) {
+            mCamsInUse++;
+        }
+
+        @Override
+        public void onCameraClosed(String cameraId) {
+            mCamsInUse--;
+        }
+    };
+
+    private boolean readCameraSoundForced() {
+        return SystemProperties.getBoolean("audio.camerasound.force", false) ||
+                mContext.getResources().getBoolean(
+                        com.android.internal.R.bool.config_camera_sound_forced);
+    }
+
 
     /**
      * Save the bitmap but don't show the normal screenshot UI.. just a toast (or notification on
